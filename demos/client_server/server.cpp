41331d69eda95f83c696bd7179f4046b2f443e2f
therealjtgill <therealjtgill@gmail.com>
2024-03-16 14:42:25 -0400

    And now we have separate client and server demos.

--- demos/client_server/server.cpp
+++ demos/client_server/server.cpp
@@ -24,19 +24,16 @@ int main(int argc, char ** argv)
       return 1;
    }
 
-   unsigned int a = 127;
-   unsigned int b = 0;
-   unsigned int c = 0;
-   unsigned int d = 1;
+   sockaddr_in from_address;
+   char packet_data[6];
 
-   unsigned int ip_add = (a << 24) | (b << 16) | ( c << 8) | d;
-
-   sockaddr_in send_address;
-   send_address.sin_family = AF_INET;
-   send_address.sin_addr.s_addr = htonl(ip_add);
-   send_address.sin_port = htons(8123);
-
-   send_packet(handle, send_address, "stuff", sizeof("stuff"));
+   while(true)
+   {
+      if (receive_packet(handle, 6, &from_address, packet_data))
+      {
+         std::cout << "packet: " << packet_data << "\n";
+      }
+   }
 
    return 0;
 }
