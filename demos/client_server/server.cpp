6a657ce851755d296ae066e130b34ef383dd45a0
therealjtgill <therealjtgill@gmail.com>
2024-03-16 22:20:11 -0400

    Update the server demo to use the new class.

--- demos/client_server/server.cpp
+++ demos/client_server/server.cpp
@@ -9,25 +9,38 @@ int main(int argc, char ** argv)
       return 0;
    }
 
-   // create the socket
-   int handle = create_socket();
+   // // create the socket
+   // int handle = create_socket();
 
-   // bind the socket
-   unsigned int bind_port = atoi(argv[1]);
+   // // bind the socket
+   // unsigned int bind_port = atoi(argv[1]);
 
-   std::cout << "port: " << bind_port << "\n";
+   // std::cout << "port: " << bind_port << "\n";
 
-   if (!bind_socket(handle, bind_port))
-   {
-      return 1;
-   }
+   // if (!bind_socket(handle, bind_port))
+   // {
+   //    return 1;
+   // }
 
    sockaddr_in from_address;
    char packet_data[6];
 
+   UdpSocket socket(atoi(argv[1]));
+
+   if (!socket.bound())
+   {
+      std::cout << "Couldn't bind socket\n";
+      return 1;
+   }
+
    while(true)
    {
-      if (receive_packet(handle, 6, &from_address, packet_data))
+      // if (receive_packet(handle, 6, &from_address, packet_data))
+      // {
+      //    std::cout << "packet: " << packet_data << "\n";
+      // }
+
+      if (socket.try_receive(6, packet_data) > 0)
       {
          std::cout << "packet: " << packet_data << "\n";
       }
