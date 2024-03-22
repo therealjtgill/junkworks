623836cc5c3ae6a2ad5f152070c15779e45f18e8
therealjtgill <therealjtgill@gmail.com>
2024-03-22 11:06:02 -0400

    Added more prints to the server output.

--- demos/counting/server_counter.cpp
+++ demos/counting/server_counter.cpp
@@ -14,7 +14,7 @@ int main(void)
 
       const auto uids = server.get_uids();
 
-      if (uids.size() > 0)
+      if (!uids.empty())
       {
          std::cout << "there are " << uids.size() << " simultaneous server connections\n";
       }
@@ -22,7 +22,14 @@ int main(void)
       for (const auto uid : uids)
       {
          const auto rx_data = server.get_client_rx_bytes(uid);
-         std::cout << "got " << rx_data.size() << " packets from uid " << uid << "\n";
+         if (!rx_data.empty())
+         {
+            std::cout << "got " << rx_data.size() << " packets from uid " << uid << "\n";
+            for (const auto & rx_bytes : rx_data)
+            {
+               std::cout << "\t" << rx_bytes[2] << " " << rx_bytes[3] << " " << rx_bytes[4] << " " << rx_bytes[5] << "\n";
+            }
+         }
       }
 
       std::this_thread::sleep_for(std::chrono::milliseconds(100));
