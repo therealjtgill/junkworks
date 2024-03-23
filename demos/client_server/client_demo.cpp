19853b211fda016c0866f8b3885c42d77ab51723
Jonathan Gill <jtgill@email.arizona.edu>
2024-03-23 04:50:35 -0400

    Updated the first client demo to use command line arguments for the server IP and server port.

--- demos/client_server/client_demo.cpp
+++ demos/client_server/client_demo.cpp
@@ -5,15 +5,24 @@
 
 int main(int argc, char ** argv)
 {
-   if (argc < 2)
+   if (argc < 6)
    {
-      std::cout << "need port number as first argument\n";
-      return 0;
+      std::cout << "server_ip0 server_ip1 server_ip2 server_ip3 client_port\n";
+      return 1;
    }
+
+   junkworks::ipv4add server_ip(
+      std::atoi(argv[1]),
+      std::atoi(argv[2]),
+      std::atoi(argv[3]),
+      std::atoi(argv[4])
+   );
+
+   const unsigned int bind_port = std::atoi(argv[5]);
 
    initialize_sockets();
 
-   junkworks::UdpSocket socket(atoi(argv[1]));
+   junkworks::UdpSocket socket(bind_port);
 
    if (!socket.bound())
    {
@@ -22,7 +31,7 @@ int main(int argc, char ** argv)
    }
 
    socket.try_send(
-      junkworks::ipv4add(127, 0, 0, 1), 8123, "stuff", sizeof("stuff")
+      server_ip, 8123, "stuff", sizeof("stuff")
    );
 
    return 0;
